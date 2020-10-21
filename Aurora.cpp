d421dcda9a471aeaef4e167a399a1411e1d8a35b
Alice <techiris09@gmail.com>
2020-10-21 10:21:30 +0200

    Updated files

--- Aurora.cpp
+++ Aurora.cpp
@@ -1,13 +1,12 @@
+#if defined(AuroraConsole)
 #include <iostream>
-#include <Core/Types.hpp>
-#include <Concepts/IsArray.hpp>
 
 int main()
 {
-    std::cout << std::boolalpha <<
-    "\nIs 'Aurora::s32' an array? " <<
-    Aurora::Concepts::IsArray<Aurora::s32> <<
-    "\nIs 'Aurora::s32[]' an array? " <<
-    Aurora::Concepts::IsArray<Aurora::s32[]>;
+    
     return 0;
 }
+#elif defined(AuroraWindows)
+
+
+#endif
